a57a4455f428d4046147b74ece7b63cdf3e08449
vincentayorinde <vincentayorinde@yahoo.com>
2019-09-05 16:28:41 +0100

    add forloop

---
+++ forloop.py
@@ -0,0 +1,33 @@
+print('*********************')
+print('BASIC STRING LIST')
+print('*********************')
+for item in ['vinay', 'tola', 'ivy']:
+  print(item)
+print('')
+
+print('*********************')
+print('BASIC NUMBER LIST')
+print('*********************')
+for item in [1, 2, 3, 4, 5]:
+  print(item)
+print('')
+
+print('*********************')
+print('BASIC RANGE LIST')
+print('*********************')
+for item in range(5):
+  print(item)
+print('')
+
+print('********************************')
+print('BASIC SPECIFIED RANGE LIST')
+print('********************************')
+for item in range(10, 15):
+  print(item)
+print('')
+
+print('*******************************************')
+print('BASIC SPECIFIED RANGE LIST WITH 2-STEPS')
+print('*******************************************')
+for item in range(10, 15, 2):
+  print(item)
